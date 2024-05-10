099ccf2bfe3c2a381bb5e01f00a85d8e4b06d8a2
Nguyen-Duc-Trong <trongnguyen160902@gmail.com>
2024-05-10 19:06:36 +0700

    update Nabar

---
+++ .history/client/src/components/DefaultLayOut/Header/Navbar/NavBar_20240510160551.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+const Navbar = () => {
+  return (
+   <div className="header-navbar w-[100%] bg-blue-500">
+        <ul className="header-list-nav flex justify-around w-[1120px] m-auto h-[40px] items-center ">
+            <Link to="/">
+                <li className=''>Trang chủ</li>
+            </Link>
+            <Link to="/favourite">
+                <li>Favourite</li>
+            </Link>
+            <Link to="/NotFoundPage">
+                <li>NotFoundPage</li>
+            </Link>
+            <Link to="/search">
+                <li>SearchPage</li>
+            </Link>
+        </ul>
+   </div>
+)
+}
+
+export default Navbar
---
+++ .history/client/src/components/DefaultLayOut/Header/Navbar/NavBar_20240510163219.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+import { FaRegHeart } from "react-icons/fa";
+const Navbar = () => {
+  return (
+   <div className="header-navbar w-[100%] bg-blue-500">
+        <ul className="header-list-nav flex justify-around w-[1120px] m-auto h-[40px] items-center text-[#ffffff] font-bold text-[14px] ">
+            <Link to="/">
+                <li className='focus:bg-[#f73859]'>Trang chủ</li>
+            </Link>
+            <Link to="/favourite">
+                <li>Homestays độc lập</li>
+            </Link>
+            <Link to="/NotFoundPage">
+                <li>Homestay nhóm</li>
+            </Link>
+            <Link to="/search">
+                <li>Homestay nghỉ dưỡng</li>
+            </Link>
+            <Link to={""}>
+                <li className='flex items-center'>
+                    <FaRegHeart className='mr-[5px] text-[15px]'/> Yêu thích
+                </li>
+            </Link>
+        </ul>
+   </div>
+)
+}
+
+export default Navbar
