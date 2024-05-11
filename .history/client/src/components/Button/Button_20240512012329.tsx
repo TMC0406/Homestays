50fd52af07bcce50060792ea028cadc05cc69b31
Nguyen-Duc-Trong <trongnguyen160902@gmail.com>
2024-05-12 01:27:56 +0700

    update pass 2

---
+++ .history/client/src/components/Button/Button_20240512012329.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = ({text, textColor, bgColor}) => {
+    return(
+        <button
+        type="button"
+        className={`py-[2px] px-[4px] ${textColor} ${bgColor} outline-none rounded-md`}
+        >
+            {text}
+        </button>
+    )
+}
+
+export default Button
