913d7263d942100e7eb3b9d27bf3a1c562490fc1
TMC0406 <cuongtran0406vn@gmail.com>
2024-05-18 01:38:11 +0700

    update

---
+++ .history/server/src/controller/auth_20240516220543.js
@@ -0,0 +1,22 @@
+import * as authService from "../service/auth"
+
+export const register  = async (req, res) => {
+    const {name, phone, password} = req.body
+    try{
+        
+        res.send('heloooo' + req.body.phone + " "+ req.body.name + " " + req.body.password)
+
+        // if (!name || !phone || !password) return res.status(400).json({
+        //     err: 1, //Lỗi số dương thì là lỗi Client còn 0 là thành công
+        //     msg: 'Missing input!'
+        // })
+        // const response =  await authService.registerService(req.body) 
+        // return res.status(200).json(response)
+
+    }catch(error) {
+        return res.status(500).json({
+            err: -1, //Lỗi số âm là lỗi Server còn 0 là thành công
+            msg: "Fall at controller: " + error
+        })
+    }
+}
---
+++ .history/server/src/routers/auth_20240516235441.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import * as authController from "../controller/auth"
+const router = express.Router()
+router.post('/register', authController.register) 
+
+
+
+
+export default router
