6f08f76e1714fe5f8d33c36408be4f1586063439
Nguyen-Duc-Trong <trongnguyen160902@gmail.com>
2024-05-18 02:03:52 +0700

    hetcuu

--- client/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterSlice from "./slides/couterSlides";
-import ChangeColor from "./slides/ChangeColor";
-export const store = configureStore({
-  reducer: {
-    counterSlice: counterSlice,
-  }
-});
